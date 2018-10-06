1739d77e09da6f66ef18f8ab8f72194fb8bfa849
lord-executor <executor.dev@gmail.com>
2018-10-06 18:33:41 +0200

    Added task to debug bundle to test variable expansion

--- utility/debug.ps1
+++ utility/debug.ps1
@@ -7,11 +7,25 @@ function Debug-Args {
     }
 }
 
+function Expand-Vars {
+    Param(
+        [string] $str,
+        $vars = @{}
+    )
+    PwshRun-ExpandVariables $str $vars
+}
+
 PwshRun-RegisterTasks "debug" @(
     @{
         Alias = "args";
         Command = "Debug-Args";
         Description = "Lists all the arguments given to the task with type information";
         Example = "`$RUNNER args [a1] [a2]";
+    },
+    @{
+        Alias = "expand";
+        Command = "Expand-Vars";
+        Description = "Lists all the arguments given to the task with type information";
+        Example = "`$RUNNER expand `"some $x of string with $variables`" @{'x' = 'form', 'variables' = 'variables'}";
     }
 )
